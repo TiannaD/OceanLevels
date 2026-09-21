ac20a4acbfed5d27f94bc3429cde939ae3d74314
TiannaD <tdacostadesign@gmail.com>
2026-09-21 14:52:19 -0400

    Added space for result output

--- OceanLevels/OceanLevels.cpp
+++ OceanLevels/OceanLevels.cpp
@@ -14,9 +14,9 @@ int main() {
 	riseIn7Years = risePerYear * 7;
 	riseIn10Years = risePerYear * 10;
 
-	cout << "Ocean level increase after 5 years: " << riseIn5Years << "mm\n";
-	cout << "Ocean level increase after 7 years: " << riseIn7Years << "mm" << endl;
-	cout << "Ocean level increase after 10 years: " << riseIn10Years << "mm" << endl;
+	cout << "Ocean level increase after 5 years: " << riseIn5Years << " mm\n";
+	cout << "Ocean level increase after 7 years: " << riseIn7Years << " mm" << endl;
+	cout << "Ocean level increase after 10 years: " << riseIn10Years << " mm" << endl;
 
 
 
